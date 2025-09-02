1ae79b0eb13bd0801956f7e028a207441d094e10
Raegan Scheet <118682668+cscheet2@users.noreply.github.com>
2025-09-02 12:59:56 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Raegan Scheet
+ * Date: 2025/09/02
+ * A simple hello program
+ * NOTE: I am a course leader doing this with a student who did not get a partner on the hack
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  printf("Hello, I am Raegan.\n");
+  printf("Major is Software Engineering!\n");
+    
+  return 0;
+}
